1f80858392df06c35b523828905071e13aa0dd3b
anant-g <35524835+anant-g@users.noreply.github.com>
2019-07-30 11:00:57 +0800

    new nuclio sdk jar

--- src/NuclioTest.java
+++ src/NuclioTest.java
@@ -1,8 +1,16 @@
+import io.nuclio.Context;
+import io.nuclio.Event;
+import io.nuclio.EventHandler;
+import io.nuclio.Response;
 
-public class NuclioTest {
+public class NuclioTest implements EventHandler {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		HelloWorld.main(args);				
+	@Override
+	public Response handleEvent(Context context, Event event) {
+		context.getLogger().infoWith("in nuclio function", "body", "in function");
+		
+		HelloWorld helloWorld = new HelloWorld();
+		
+		return new Response().setBody(helloWorld.testCall());
 	}
 }
